bee675b4dd1a8c7ddb8094cae5eb1cc6ba81407f
Christoffer Cederfeldt <ZebulonZCC@gmail.com>
2020-02-13 11:03:35 +0100

    GameController
    Added perspective change event that other objects can subscribe to!

--- Unity/Assets/Scripts/GameController.cs
+++ Unity/Assets/Scripts/GameController.cs
@@ -1,25 +1,45 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
+public enum Perspective { None, Angela, Elenor }
 public class GameController : MonoBehaviour
 {
     public static GameController get = null;
+    public UnityAction<GameController, Perspective> onChangePerspective = delegate { };
+    [SerializeField] Perspective currentPerspective = Perspective.None;
+    public bool skipNone = true;
+    public Perspective CurrentPerspective
+    {
+        get
+        {
+            return currentPerspective;
+        }
+        set
+        {
+            value = (Perspective)Mathf.Repeat((int)value, Enum.GetNames(typeof(Perspective)).Length);
+            if (value == 0 && skipNone)
+                value = Perspective.Angela;
+            if (currentPerspective != value)
+            {
+                currentPerspective = value;
+                onChangePerspective?.Invoke(this, currentPerspective);
+            }
+        }
+    }
+    public KeyCode switchKey = KeyCode.Tab;
+
     private void Awake()
     {
         get = this;
     }
-
-    public void Test()
-    {
-        print("Hello");
-    }
-
     private void Update()
     {
         QuitGame();
+        Switch();
     }
-
     void QuitGame()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -29,6 +49,13 @@ public class GameController : MonoBehaviour
 #else
       Application.Quit();
 #endif
+        }
+    }
+    void Switch()
+    {
+        if (Input.GetKeyDown(switchKey))
+        {
+            CurrentPerspective += 1;
         }
     }
 }
