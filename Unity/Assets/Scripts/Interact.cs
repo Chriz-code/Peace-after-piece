81591d1861e2efb0c38ea5f28b6ffb9e3911d679
Christoffer Cederfeldt <ZebulonZCC@gmail.com>
2020-02-13 14:13:04 +0100

    Added ThisPerspective

--- Unity/Assets/Scripts/Interact.cs
+++ Unity/Assets/Scripts/Interact.cs
@@ -54,6 +54,10 @@ public class Interact : MonoBehaviour
     {
         if (collision.transform.GetComponent<Player>())
             Interactable = false;
+    }
 
+    private void OnDestroy()
+    {
+        Interactable = false;
     }
 }
---
+++ Unity/Assets/Scripts/ThisPerspective.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThisPerspective : MonoBehaviour
+{
+    public Perspective perspective = Perspective.Angela;
+}
