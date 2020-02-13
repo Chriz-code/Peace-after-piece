30b73574eac8598798bde27e4d41efd6b641618e
Judith <judithgammelli@gmail.com>
2020-02-13 14:42:07 +0100

    Auto stash before merge of "Programming" and "origin/Programming"

---
+++ Unity/Assets/Scripts/Item.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public GameObject player;
+    private bool pickUpAllowed;
+
+    private Inventory inventory;
+    public GameObject itemButton;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
+    private void Update()
+    {
+        if (pickUpAllowed && Input.GetKeyDown(KeyCode.E))
+            PickUpItem();
+    }
+
+    private void PickUpItem()
+    {
+        for (int i = 0; i < inventory.slots.Length; i++)
+        {
+            if (inventory.isFull[i] == false)
+            {
+                inventory.isFull[i] = true;
+                Instantiate(itemButton, inventory.slots[i].transform, false);
+                Destroy(gameObject);
+                break;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            pickUpAllowed = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            pickUpAllowed = false;
+    }
+}
