7fc71d5bb26ebf5c3c43eb08551cd61c5e7f4d15
Christoffer Cederfeldt <ZebulonZCC@gmail.com>
2020-02-21 15:59:00 +0100

    En bra ny start på inventory

--- Unity/Assets/Scripts/Inventory.cs
+++ Unity/Assets/Scripts/Inventory.cs
@@ -12,40 +12,51 @@ public class Inventory : MonoBehaviour
         get
         {
             if (slot.item == null)
+            {
+                Debug.Log("HEJ");
                 return true;
+            }
             return false;
         }
     }
     public Slot slot;
-
+    public LayerMask mask;
     Item CheckItem()
     {
-        RaycastHit2D[] raycastHit2Ds = Physics2D.BoxCastAll(transform.position, GetComponent<BoxCollider2D>().size, 0, Vector2.zero);
+        Vector3 boxCastRayGrej = new Vector3(
+            Mathf.Abs(GetComponent<BoxCollider2D>().size.x * transform.localScale.x),
+            Mathf.Abs(GetComponent<BoxCollider2D>().size.y * transform.localScale.y),
+            1);
 
-        if (raycastHit2Ds != null)
+        Collider[] collider2Ds = Physics.OverlapBox(transform.position, boxCastRayGrej, Quaternion.identity, mask);
+
+        if (collider2Ds.Length > 0)
         {
-            for (int i = 0; i < raycastHit2Ds.Length; i++)
+            for (int i = 0; i < collider2Ds.Length; i++)
             {
-                    Debug.Log("Hola! "+ raycastHit2Ds[i].collider.name);
-                if (raycastHit2Ds[i].collider.GetComponent<Item>())
+                //Debug.Log("Hola! " + collider2Ds[i].name);
+                if (collider2Ds[i].GetComponent<Item>())
                 {
-                    return raycastHit2Ds[i].collider.GetComponent<Item>();
-
+                    return collider2Ds[i].GetComponent<Item>();
                 }
             }
         }
-        Debug.Log("Kitos!");
+        //Debug.Log("Kitos!");
         return null;
     }
 
     public void PickUpItem()
     {
-        if (!PickUpAllowed)
+
+
+        if (PickUpAllowed == false)
             return;
 
-        Item item = CheckItem();
+        //Item item = CheckItem();
         if (item == null)
             return;
+
+
         slot.item = item;
         slot.GetComponent<UnityEngine.UI.Image>().sprite = item.GetComponent<SpriteRenderer>().sprite;
         //itembutton = Instantiate(itembuttonPrefab, inventory.slots[i].transform, false);
@@ -55,4 +66,25 @@ public class Inventory : MonoBehaviour
     {
         gameObject.GetComponent<Transform>().localPosition = player.GetComponent<Transform>().localPosition + Vector3.forward;
     }
+
+
+    public Item item = null;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Item>())
+        {
+            item = collision.GetComponent<Item>();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Item>())
+        {
+            item = null;
+        }
+    }
+
+
+
 }
---
+++ Unity/Assets/Shader/Shockwave/ShockwaveEffect.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShockwaveEffect : MonoBehaviour
+{
+    public Shader shader;
+
+    public AnimationCurve defaultCurve;
+    public AnimationCurve _AnnulusMin, _MaxRange, _DistortionStrength;
+    public float speed = 1;
+
+    private Material m_Material;
+    Material material
+    {
+        get
+        {
+            if (m_Material == null)
+            {
+                m_Material = new Material(shader);
+                m_Material.hideFlags = HideFlags.HideAndDontSave;
+            }
+            return m_Material;
+        }
+    }
+
+    public void ShockWaveEffect()
+    {
+
+    }
+    public void ShockWaveEffect(AnimationCurve _Annulus, AnimationCurve _MaxRange, AnimationCurve _DistortionStrenght)
+    {
+
+    }
+
+    public IEnumerator ApplyEffect(AnimationCurve _Annulus, AnimationCurve _MaxRange, AnimationCurve _DistortionStrenght)
+    {
+        if (_Annulus == null)
+            _Annulus = defaultCurve;
+        if (_MaxRange == null)
+            _MaxRange = defaultCurve;
+        if (_DistortionStrenght == null)
+            _DistortionStrenght = defaultCurve;
+
+        for (float i = 0; i < 1; i += Time.deltaTime * speed)
+        {
+            //"Annulus Radius"
+            material.SetFloat("Annulus Radius", _Annulus.Evaluate(i));
+            //"Outer Radius"
+            material.SetFloat("Outer Radius", _MaxRange.Evaluate(i));
+            //"Distortion Strength"
+            material.SetFloat("Distortion Strength", _DistortionStrenght.Evaluate(i));
+
+            yield return new WaitForSeconds(Time.deltaTime * speed);
+        }
+    }
+}
