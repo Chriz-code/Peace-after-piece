39b4ea10bc53d10cfcdf03f571e8c5166fb10add
Judith <judithgammelli@gmail.com>
2020-03-12 14:57:02 +0100

    Sprite Switch
    Jag stal Judith dator och fixade med sprite switch

--- Unity/Assets/Scripts/Item.cs
+++ Unity/Assets/Scripts/Item.cs
@@ -8,59 +8,12 @@ public class Item : MonoBehaviour
 {
     public Transform parent = null;
     public ItemType itemType = ItemType.Book;
-    public Sprite book = null, key = null, painting = null;
+    [Header("OverWorldSprites")]
+    public Sprite defaultSprite = null;
+    public Sprite changeSprite = null;
 
-    //public GameObject player;
-    //private bool pickUpAllowed;
-
-    //private Inventory inventory;
-    //public GameObject itemButton;
-
-    //private void Start()
-    //{
-    //    inventory = player.GetComponent<Inventory>();
-    //}
-
-    //private void Update()
-    //{
-    //  if (Input.GetKeyDown(KeyCode.Q))
-    //DropItem();
-
-    //    }
-
-    //public void PickUpItem()
-    //{
-    //    if (!pickUpAllowed)
-    //        return;
-
-    //    for (int i = 0; i < inventory.slots.Length; i++)
-    //    {
-    //        if (inventory.isFull[i] == false)
-    //        {
-    //            inventory.isFull[i] = true;
-    //            Instantiate(itemButton, inventory.slots[i].transform, false);
-    //            gameObject.GetComponent<Transform>().localPosition = new Vector2(222, 222);
-    //            //Destroy(gameObject);
-    //            break;
-    //        }
-    //    }
-    //}
-
-    //public void DropItem()
-    //{
-    //    gameObject.GetComponent<Transform>().localPosition = player.GetComponent<Transform>().localPosition + Vector3.forward;
-    //}
-    /*
-        private void OnTriggerEnter2D(Collider2D collision)
-        {
-            if (collision.gameObject.CompareTag("Player"))
-                pickUpAllowed = true;
-        }
-
-        private void OnTriggerExit2D(Collider2D collision)
-        {
-            if (collision.gameObject.CompareTag("Player"))
-                pickUpAllowed = false;
-        }
-        */
+    [Header("InspectImage")]
+    public Sprite book = null;
+    public Sprite key = null;
+    public Sprite painting = null;
 }
