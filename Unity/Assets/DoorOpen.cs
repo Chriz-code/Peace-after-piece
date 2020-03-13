540d8e0de3392c51842ac91a1a5e1a1ed9c7002b
Judith <judithgammelli@gmail.com>
2020-03-13 11:13:36 +0100

    Auto stash before merge of "Programming" and "origin/Programming"

---
+++ Unity/Assets/DoorOpen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    public Sprite open;
+    public Sprite closed;
+
+    public void SwitchSprite()
+    {
+        Debug.Log("opendoor!");
+
+        if (GetComponent<SpriteRenderer>().sprite == closed)
+            GetComponent<SpriteRenderer>().sprite = open;     
+        if(GetComponent<SpriteRenderer>().sprite == open)
+            GetComponent<SpriteRenderer>().sprite = closed;
+
+    }
+}
