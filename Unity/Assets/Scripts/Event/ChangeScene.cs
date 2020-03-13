62a8085d9ea78490cdb80b7395d60ef80acb7b6b
Judith <judithgammelli@gmail.com>
2020-03-13 11:31:36 +0100

    sceneswitch

--- Unity/Assets/Scripts/Event/ChangeScene.cs
+++ Unity/Assets/Scripts/Event/ChangeScene.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class ChangeScene : MonoBehaviour
 {
+    public int overrideIndex = 0;
+
     public void ChangeSceneInt(int i)
     {
         GameController.Get.GetComponent<SceneController>().ChangeScene(i);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Player>())
+            ChangeSceneInt(overrideIndex);
     }
 }
