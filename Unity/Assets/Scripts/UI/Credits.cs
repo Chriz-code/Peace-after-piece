2707b2f9d0e3756eeb61811ed18c9c7945c1c00e
Judith <judithgammelli@gmail.com>
2020-03-16 12:21:51 +0100

    Credits

---
+++ Unity/Assets/Scripts/UI/Credits.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+    public GameObject camera;
+    public int speed = 1;
+    public string meny;
+
+    void Update()
+    {
+        camera.transform.Translate(Vector3.down * Time.deltaTime * speed);
+
+        if(camera.GetComponent<Transform>().position.y == -90)
+        {
+            SceneManager.LoadScene(meny)
+        }
+    }
+}
